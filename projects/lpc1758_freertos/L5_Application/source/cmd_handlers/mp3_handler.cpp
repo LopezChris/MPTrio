f8fcfbdc46bfd7f3677105474b160a4adc682523
powerbygood <powerbygood@yahoo.com>
2018-12-18 03:14:05 -0800

    ISR Interrupt, not sure if implementation is correct

--- projects/lpc1758_freertos/L5_Application/source/cmd_handlers/mp3_handler.cpp
+++ projects/lpc1758_freertos/L5_Application/source/cmd_handlers/mp3_handler.cpp
@@ -1,28 +1,23 @@
 #include "tasks.hpp"
 #include "uart0_min.h"
+#include "queue.h"
+//#include "FreeRTOS.h"
+#include "gpio.hpp"
+#include "LabGPIOInterrupts.hpp"
 
-bool mp3PlayerHandler(str& cmdParams, CharDev& output, void* pDataParam) {
-
-    const char *filename = cmdParams.c_str();
-
-    mp3PlayerTask mp3(3);
-    mp3.initCodec();
-    //while (true) {
-    //mp3.sineTest();
-    //}
-    mp3.playFile(std::string(filename));
-    return true;
+QueueHandle_t task_queue = scheduler_task::getSharedObject("mp3_cmd_queue");
+void create_q()
+{
+    if (task_queue == NULL) {
+         task_queue = xQueueCreate(16, sizeof(mp3Command));
+         scheduler_task::addSharedObject("mp3_cmd_queue", task_queue);
+         uart0_puts("Initialized queue");
+     }
 }
 
 void send_mp3_cmd(mp3Command toSend) {
-    QueueHandle_t task_queue = scheduler_task::getSharedObject("mp3_cmd_queue");
-    if (task_queue == NULL) {
-        task_queue = xQueueCreate(16, sizeof(mp3Command));
-        scheduler_task::addSharedObject("mp3_cmd_queue", task_queue);
-        uart0_puts("Initialized queue");
-    }
 
-    xQueueSend(task_queue, &toSend, 0);
+        xQueueSendFromISR(task_queue, &toSend, NULL);
 }
 
 bool mp3PlayerNextHandler(str& cmdParams, CharDev& output, void* pDataParam) {
@@ -51,3 +46,66 @@ bool mp3PlayerResumeHandler(str& cmdParams, CharDev& output, void* pDataParam) {
 
     return true;
 }
+
+void prev()
+{
+    GPIO Prev(P1_22);
+    Prev.setAsInput();
+
+    send_mp3_cmd(mp3Command::PREV);
+}
+
+void pause()
+{
+    GPIO Pause(P1_23);
+
+
+    Pause.setAsInput();
+
+
+    send_mp3_cmd(mp3Command::PAUSE);
+
+
+
+}
+
+void next()
+{
+    GPIO Next(P1_28);
+    Next.setAsInput();
+}
+void init()
+{
+   // LabGpioInterrupts gpio_interrupt;
+
+    //QueueHandle_t task_queue = scheduler_task::getSharedObject("mp3_cmd_queue");
+
+   // GPIO Prev(P1_22);
+   /// GPIO Pause(P1_23);
+   // GPIO Next(P1_28);
+
+   // Prev.setAsInput();
+   // Pause.setAsInput();
+   // Next.setAsOutput();
+
+    gpio_interrupt.Initialize();
+    gpio_interrupt.AttachInterruptHandler(1,23,&pause,kFallingEdge);
+    gpio_interrupt.AttachInterruptHandler(1,22,&prev,kFallingEdge);
+
+}
+
+bool mp3PlayerHandler(str& cmdParams, CharDev& output, void* pDataParam) {
+
+    const char *filename = cmdParams.c_str();
+
+    mp3PlayerTask mp3(3);
+    mp3.initCodec();
+    create_q();
+    init();
+    //while (true) {
+    //mp3.sineTest();
+    //}
+    mp3.playFile(std::string(filename));
+    return true;
+}
+
